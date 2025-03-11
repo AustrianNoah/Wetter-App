ef932dba5f443830b3384dd12a283d5a5cb6f7d5
JavaSnippets <marcel.w@friendlyhost.email>
2025-03-11 12:06:46 +0100

    added an about page without css

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Willkommen</h1>
+        <p>Dies ist eine einfache Seite ohne CSS-Imports.</p>
+      </div>
+    )
+  }
+  
